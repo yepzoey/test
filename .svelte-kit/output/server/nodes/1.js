0e0dc1d1f7b8780500f9fd71abe138abbc34dbce
Zoey Zhao <68380548+yepzoey@users.noreply.github.com>
2025-02-22 22:33:10 -0800

    Removed node_modules and .svelte-kit from Git tracking

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Bdez-2aR.js","_app/immutable/chunks/Bg9kRutz.js","_app/immutable/chunks/69_IOA4Y.js","_app/immutable/chunks/DIeogL5L.js","_app/immutable/chunks/Bm0FhLEK.js","_app/immutable/chunks/C92lvqdm.js","_app/immutable/chunks/CHZwxh0T.js","_app/immutable/chunks/BQPxy4xd.js","_app/immutable/chunks/CMB7kT7n.js","_app/immutable/chunks/HxPqkZ8F.js","_app/immutable/chunks/eP3un58F.js"];
-export const stylesheets = [];
-export const fonts = [];

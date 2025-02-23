c5a5600439d980491b866faa5bad735f99751b3a
Zoey Zhao <68380548+yepzoey@users.noreply.github.com>
2025-02-22 22:27:35 -0800

    Removed .svelte-kit from repo (it's in .gitignore)

--- .svelte-kit/output/server/entries/pages/references/_page.svelte.js
+++
@@ -1,8 +0,0 @@
-import "clsx";
-/* empty css                     */
-function _page($$payload) {
-  $$payload.out += `<section id="references"><h1>References</h1> <p>Here are the key references used in this project:</p> <ul><li>Heidari, M. et al. “Improving the performance of CNN to predict the likelihood of COVID-19 using chest X-ray images with preprocessing algorithms.” International Journal of Medical Informatics, 144 (2020): 104284. doi:10.1016/j.ijmedinf.2020.104284</li> <li>Jadwaa, S. K. “X-Ray Lung Image Classification Using a Canny Edge Detector.” Journal of Electrical and Computer Engineering, vol. 2022, 3081584, 8 pages, 2022. doi:10.1155/2022/3081584</li> <li>Rajpurkar, P. et al. “CheXNet: Radiologist-Level Pneumonia Detection on Chest X-Rays with Deep Learning.” arXiv preprint arXiv:1711.05225 (2017). https://arxiv.org/abs/1711.05225</li></ul></section> <aside><div class="contents-header">On this page</div> <ul><li><a href="#references">References</a></li></ul></aside>`;
-}
-export {
-  _page as default
-};
--- .svelte-kit/output/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/dataset/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.CG42OUbp.js","_app/immutable/chunks/Bg9kRutz.js","_app/immutable/chunks/69_IOA4Y.js","_app/immutable/chunks/DIeogL5L.js","_app/immutable/chunks/Bm0FhLEK.js","_app/immutable/chunks/BQPxy4xd.js","_app/immutable/chunks/F2zvKX18.js","_app/immutable/chunks/CHZwxh0T.js","_app/immutable/chunks/CMB7kT7n.js","_app/immutable/chunks/eP3un58F.js","_app/immutable/chunks/C92lvqdm.js","_app/immutable/chunks/Cy-pn9s0.js"];
-export const stylesheets = ["_app/immutable/assets/4.CMibUpoG.css","_app/immutable/assets/global.DuPibpby.css"];
-export const fonts = [];

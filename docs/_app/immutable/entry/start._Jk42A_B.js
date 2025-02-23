816a35a8130c0a988698be7d5ac96ec06be8cb34
Zoey Zhao <68380548+yepzoey@users.noreply.github.com>
2025-02-22 22:40:12 -0800

    Rebuild with correct base path

---
+++ docs/_app/immutable/entry/start._Jk42A_B.js
@@ -0,0 +1,1 @@
+import{b as a}from"../chunks/DrSBNTWd.js";export{a as start};
--- website/svelte.config.js
+++ website/svelte.config.js
@@ -3,14 +3,14 @@ import adapter from '@sveltejs/adapter-static';
 const dev = process.env.NODE_ENV === 'development';
 
 export default {
-	kit: {
-	  adapter: adapter({
-		pages: 'docs',
-		assets: 'docs',
-		fallback: '404.html',
-	  }),
-	  paths: {
-		base: dev ? '' : '/test',
-	  }
-	},
-  };
+  kit: {
+    adapter: adapter({
+      pages: '../docs',
+      assets: '../docs',
+      fallback: '404.html',
+    }),
+    paths: {
+      base: dev ? '' : '/test',
+    }
+  },
+};
